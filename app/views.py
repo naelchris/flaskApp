cfb9c5c137f82ac92cf099b5982a2e62d69a4467
naelchris <naelchris@gmail.com>
2021-05-27 20:19:40 +0700

    initial flask app

---
+++ app/views.py
@@ -0,0 +1,94 @@
+from app import app
+from flask import render_template
+
+
+@app.route("/")
+def index():
+    return "hello world"
+
+@app.route("/jinja")
+def jinja():
+    
+    #string
+    my_name = "Julian"
+    my_age = 30
+    langs = ["Python","Javascript"]
+    friends= {
+        "Tony": 43,
+        "Cody": 28,
+        "Amy": 26,
+        "Clarissa": 23,
+        "Wendell": 39
+    }
+
+    colors = ("Red", "Blue")
+
+    # Booleans
+    cool = True
+
+    #class 
+    class GitRemote:
+        def __init__(self, name, description, domain):
+            self.name = name
+            self.description = description
+            self.domain = domain
+
+        def pull(self):
+            return f"Pulling repo '{self.name}'"
+
+        def clone(self, repo):
+            return f"Cloning into {repo}"
+
+    my_remote = GitRemote(
+        name="Learning Flask",
+        description="Learn the Flask web framework for Python",
+        domain="https://github.com/Julian-Nash/learning-flask.git"
+    )
+
+    # Functions
+    def repeat(x, qty=1):
+        return x * qty
+
+    return render_template("jinja/jinja.html", 
+        my_name=my_name, my_age=my_age, langs=langs,
+        friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, 
+        my_remote=my_remote, repeat=repeat
+    )
+
+@app.route("/about")
+def about():
+    return  """ 
+    <h1 style='color: red;'>I'm a red H1 heading!</h1>
+    <p>This is a lovely little paragraph</p>
+    <code>Flask is <em>awesome</em></code>
+    """
+
+@app.route("/profile/<username>")
+def profile(username):
+
+    users = {
+    "mitsuhiko": {
+        "name": "Armin Ronacher",
+        "bio": "Creatof of the Flask framework",
+        "twitter_handle": "@mitsuhiko"
+    },
+    "gvanrossum": {
+        "name": "Guido Van Rossum",
+        "bio": "Creator of the Python programming language",
+        "twitter_handle": "@gvanrossum"
+    },
+    "elonmusk": {
+        "name": "Elon Musk",
+        "bio": "technology entrepreneur, investor, and engineer",
+        "twitter_handle": "@elonmusk"
+    }
+    }
+
+    user = None
+
+    if(username in users):
+        user = users[username]
+
+    print(username)
+    print(user)
+    return render_template("profile/profile.html", username=username, user=user)
